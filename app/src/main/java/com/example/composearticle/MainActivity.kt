dd3e77cbfd5e2bf117039a0755e9b0c33f6bae25
HaiNinh <luongvuhaininh123@gmail.com>
2023-01-16 17:14:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.composearticle
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+//                    Greeting("Android")
+                    ComposeArticleApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticleApp(){
+    ArticleCard(
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+
+) {
+    Column(modifier = modifier) {
+        Image(painter=imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding( 16.dp)
+        )
+
+        Text(
+            text = shortDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+
+        Text(
+            text = longDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+/*
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+*/
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeArticleTheme {
+//        Greeting("Android")
+        Surface{
+            ComposeArticleApp()
+        }
+    }
+}
